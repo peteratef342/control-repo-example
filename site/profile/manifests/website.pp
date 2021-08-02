a04f13d2288d0210d6af23030b8fd4b19362be3f
AWS OpsWorks CM <info@amazonaws.com>
2021-08-02 12:48:28 +0000

    OpsWorks CM StarterKit

---
+++ site/profile/manifests/website.pp
@@ -0,0 +1,17 @@
+class profile::website {
+
+  include 'apache'
+
+  apache::vhost { 'localhost':
+    port    => '8080',
+    docroot => '/var/www/demo-website',
+  }
+
+  file { [ '/var/www',
+           '/var/www/demo-website', ]:
+    ensure => directory,
+    recurse => 'true',
+    mode    => '0755',
+    source  => 'puppet:///modules/profile/demo-website/',
+  }
+}
